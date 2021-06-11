a58c34d161e957fcd5df240f75f8558d24649569
Mateusz Wszola <wszola.m@gmail.com>
2021-06-11 21:01:18 +0200

    Add react error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from '../components/ErrorFallback';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextAuthProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </NextAuthProvider>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <NextAuthProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </NextAuthProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 export default MyApp;
